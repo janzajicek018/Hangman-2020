614c85e5d4b7a07dd593ddb9b72f2b748bd4172d
Jan Zajíček <u18.430@pslib.cloud>
2020-06-04 14:38:04 +0200

    commit

---
+++ Hangman/Services/WordLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangman.Services
+{
+    public class WordLogic
+    {
+        public List<string> Sport { get; } = new List<string>() { };
+        public List<string> Culture { get; } = new List<string>() { };
+        public List<string> Music { get; } = new List<string>() { };
+        public List<string> Movies { get; } = new List<string>() { };
+    }
+}
